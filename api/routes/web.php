a25d20894e960a9aaca45e483ecfedbb280b896c
Łukasz Zychal <zychallukasz@gmail.com>
2025-11-06 11:54:36 +0100

    fix: dodanie route / do api.php i test route /2 w web.php
    Zmiany:
    - Dodano route / z welcome payload do routes/api.php
    - Zmieniono route / na /2 w routes/web.php (test)
    - Laravel Pint auto-fix: dodano brakujące puste linie na końcu plików
    
    Test:
    - Route / w api.php powinien działać (api routes są ładowane przed web routes)
    - Route /2 w web.php do testowania
    - Wszystkie route będą poprawnie cache'owane z OPcache reset

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/2', function () {
     return response()->json([
         'name' => config('app.name', 'MovieMind API'),
         'version' => '1.0.0',
